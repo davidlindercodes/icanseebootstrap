a7c2681221b598dfd125455a62db4320e593762d
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2021-04-28 01:07:48 +0700

    🚀 Header done

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const doneBtn = document.querySelector(".doneBtn");
+const alert = document.querySelector(".alert");
+
+doneBtn.addEventListener("click", hideAlert);
+
+function hideAlert() {
+  alert.classList.add("hide");
+}
