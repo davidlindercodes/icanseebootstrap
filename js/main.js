4204dcede58488ad8f3243480ba5f438887d74bd
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2021-04-28 21:22:10 +0700

    :rocket: Notifications now funcitoning as expected

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const doneBtn = document.querySelector(".doneBtn");
 const alert = document.querySelector(".alert");
+const showmore = document.querySelector(".showMore");
+const more = document.querySelector(".more");
 
-doneBtn.addEventListener("click", hideAlert);
+
+
 
 function hideAlert() {
   alert.classList.add("hide");
 }
 
+function showMore() {
+ // showmore.classList.add("hide");
+document.getElementById("showMore").innerHTML = "<b>Show Less</b>";
+  more.classList.toggle("hide");
+}
 
+// function showLess() {
+//   more.classList.add("hide");
+//   showmore.classList.remove("hide");
+// }
